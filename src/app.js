6fbafb116bbdfb11839ad824f2abd2cb44026093
Abhishek Gupta <abhishek.gupta_cs18@gl.ac.in>
2021-04-16 11:38:55 -0400

    my first commit

--- src/app.js
+++ src/app.js
@@ -24,7 +24,113 @@ app.get("/",(req,res)=>{
     res.render("index");
 })
 
-app.get("/signup",(req,res)=>{
+app.get("/about",(req,res)=>{
+    res.render("about");
+}) 
+
+app.get("/BOOT",(req,res)=>{
+    res.render("BOOT");
+})
+
+app.get("/cart",(req,res)=>{
+    res.render("cart");
+})
+
+app.get("/checkout",(req,res)=>{
+    res.render("checkout");
+})
+
+app.get("/CHOCOLATE",(req,res)=>{
+    res.render("CHOCOLATE");
+})
+
+app.get("/CLEAN",(req,res)=>{
+    res.render("CLEAN");
+})
+
+app.get("/contact-us",(req,res)=>{
+    res.render("contact-us");
+})
+app.get("/daily",(req,res)=>{
+    res.render("daily");
+})
+app.get("/dailyitem",(req,res)=>{
+    res.render("dailyitem");
+})
+app.get("/DAL",(req,res)=>{
+    res.render("DAL");
+})
+app.get("/EATABLE",(req,res)=>{
+    res.render("EATABLE");
+})
+app.get("/elecitem",(req,res)=>{
+    res.render("elecitem");
+})
+app.get("/electronic",(req,res)=>{
+    res.render("electronic");
+})
+app.get("/FOODESS",(req,res)=>{
+    res.render("FOODESS");
+})
+app.get("/gallery",(req,res)=>{
+    res.render("gallery");
+})
+app.get("/HEAD",(req,res)=>{
+    res.render("HEAD");
+})
+app.get("/JACKETS",(req,res)=>{
+    res.render("JACKETS");
+})
+app.get("/JEANS",(req,res)=>{
+    res.render("JEANS");
+})
+app.get("/laptop",(req,res)=>{
+    res.render("laptop");
+})
+app.get("/ledtv",(req,res)=>{
+    res.render("ledtv");
+})
+app.get("/lifeitem",(req,res)=>{
+    res.render("lifeitem");
+})
+app.get("/lifestyle",(req,res)=>{
+    res.render("lifestyle");
+})
+app.get("/mobile",(req,res)=>{
+    res.render("mobile");
+})
+app.get("/my-account",(req,res)=>{
+    res.render("my-account");
+})
+app.get("/REFRI",(req,res)=>{
+    res.render("REFRI");
+})
+app.get("/SHOE",(req,res)=>{
+    res.render("SHOE");
+})
+app.get("/shop-detail",(req,res)=>{
+    res.render("shop-detail");
+})
+app.get("/shop",(req,res)=>{
+    res.render("shop");
+})
+app.get("/ST",(req,res)=>{
+    res.render("ST");
+})
+app.get("/SUIT",(req,res)=>{
+    res.render("SUIT");
+})
+app.get("/VEG",(req,res)=>{
+    res.render("VEG");
+})
+app.get("/WASH",(req,res)=>{
+    res.render("WASH");
+})
+app.get("/wishlist",(req,res)=>{
+    res.render("wishlist");
+})
+app.get("/signup",(req,
+    res)=>{
     res.render("signup");
 })
 
@@ -61,7 +167,7 @@ app.post("/loginn",async(req,res)=>{
         const password=req.body.password
         const useremail=await Register.findOne({email:email});
         if(useremail.password===password){
-            res.status(201).render("signup");
+            res.status(201).render("index");
         }
         else{
             res.send("Invalid login details");
